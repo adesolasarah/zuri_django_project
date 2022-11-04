7c1c56fbb3d73b6fe6856d0dd3704e8dd44155cd
adesolasarah <tumih212@gmail.com>
2022-11-04 13:03:43 +0100

    Added changes

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name +  " " + self.last_name
+
+class Song(models.Model):
+    title = models.CharField(max_length=120)
+    date_released = models.DateField(max_length=120)
+    likes = models.IntegerField()
+    artiste_id= models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.date_released:
+            return f"{self.title} ({self.date_released})"
+        return self.title
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=120)
+    song_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+  
--- songcrud/settings.py
+++ songcrud/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # add these lines: 
+    'django_extensions',
+    'musicapp.apps.MusicappConfig',
 ]
 
 MIDDLEWARE = [
